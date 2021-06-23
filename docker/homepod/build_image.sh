036bd17b0455c17dade08be0cacf0f7c23613930
Gemfield <gemfield@civilnet.cn>
2021-06-23 08:04:10 +0000

    add tensorrt to homepod 2.0

--- docker/homepod/build_image.sh
+++ docker/homepod/build_image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
 CUDA_VER="11.1.1"
-docker build -t gemfield/homepod:$CUDA_VER-cudnn8-devel-ubuntu20.04 -f Dockerfile.pro .
+docker build -t gemfield/homepod:$CUDA_VER-cudnn8-devel-ubuntu20.04 -f Dockerfile .
+#docker build -t gemfield/homepod:$CUDA_VER-cudnn8-devel-ubuntu20.04-pro -f Dockerfile.pro .
